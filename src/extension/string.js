dc09852c13d1cd72fd71c8fa8f97d6f9f7dc198a
Tlahey <a.vendeville@orange.fr>
2017-04-17 20:20:32 +0200

    Delete useless files

--- src/extension/string.js
+++
@@ -1,8 +0,0 @@
-String.prototype.format = function () {
-    var formatted = this;
-    for (var i = 0; i < arguments.length; i++) {
-        formatted = formatted.replace(RegExp("\\{" + i + "\\}", 'g'), arguments[i].toString());
-    }
-    return formatted;
-};
-//# sourceMappingURL=string.js.map
